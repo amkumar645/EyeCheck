d0ef606ec1fc2642adc5adeb2c64da06d7052f99
Arnav Kumar <amkumar@princeton.edu>
2022-07-08 19:23:28 -0400

    Home screen designed

--- App.js
+++ App.js
@@ -1,20 +1,121 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView} from 'react-native';
+import { colors } from './components/utils/colors';
+import logo from './assets/logo.png';
+import { Entypo, FontAwesome5 } from '@expo/vector-icons';
+import * as Font from "expo-font";
+import Apploading from "expo-app-loading";
+import React, { useState } from "react";
+import { Platform } from 'react-native';
+
+const getFonts = () =>
+  Font.loadAsync({
+    'Copperplate': require('./assets/fonts/copperplate.ttf'),
+    'CopperplateBold': require('./assets/fonts/copperplatebold.ttf'),
+  });
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontsloaded, setFontsLoaded] = useState(false);
+
+  if (fontsloaded) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Text style={styles.title}>EyeCheck</Text>
+        </View>
+        <Image
+          style={styles.logo}
+          source={logo}
+        />
+        <View style={styles.sectionBox}>
+          <Text style={styles.section}>        
+            <FontAwesome5 style={styles.icon} name={'book-open'}/>
+            <Text>{" "}</Text>
+            History
+          </Text>
+        </View>
+        <View style={styles.sectionBox}>
+          <Text style={styles.section}>        
+            <Entypo style={styles.icon} name={'magnifying-glass'}/>
+            <Text>{" "}</Text>
+            Exam
+          </Text>
+        </View>
+        <View style={styles.sectionBox}> 
+          <Text style={styles.section}>        
+            <FontAwesome5 style={styles.icon} name={'eye-dropper'}/>
+            <Text>{" "}</Text>
+            Pharmacy
+          </Text>
+        </View>
+        <View style={styles.sectionBox}>
+          <Text style={styles.section}>        
+            <FontAwesome5 style={styles.icon} name={'question'}/>
+            <Text>{"  "}</Text>
+            Glossary
+          </Text>
+        </View>
+        <View>
+          <Text style={styles.footer}>ABOUT</Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
+
+  else {
+    return (
+      <Apploading
+        startAsync={getFonts}
+        onFinish={() => {
+          setFontsLoaded(true);
+        }}
+        onError={console.warn}
+      />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.bgwhite,
     alignItems: 'center',
     justifyContent: 'center',
+    
+  },
+  footer: {
+    fontSize: Platform.OS === "android" ? 20: 25,
+    color: colors.darkblue,
+    fontFamily: 'Copperplate',
+    marginTop: 20,
+  },
+  icon: {
+    fontSize: Platform.OS === "android" ? 30: 40,
+  },
+  logo: {
+    height: 172,
+    width: 154,
+    marginBottom: 30,
+  },
+  section: {
+    fontSize: Platform.OS === "android" ? 30: 40,
+    color: colors.darkblue,
+    marginLeft: 10, 
+    fontFamily: 'Copperplate',
+  },
+  sectionBox: {
+    borderWidth: 2,
+    borderColor: colors.darkblue,
+    backgroundColor: colors.lightblue,
+    borderRadius: 5,
+    width: 323,
+    height: 84,
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: Platform.OS === "android" ? 50: 60,
+    marginBottom: 30,
+    color: colors.darkerblue,
+    fontFamily: 'Copperplate',
   },
 });
---
+++ components/utils/colors.js
@@ -0,0 +1,6 @@
+export const colors = {
+    'bgwhite': '#f5f5f5',
+    'darkerblue': '#083145',
+    'darkblue': '#126e9a',
+    'lightblue': '#f2f6f8'
+}
